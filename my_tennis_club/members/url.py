d17796824972c7596c06d251061a6dbe945dd754
Daniel <122674836+DanielBucayan@users.noreply.github.com>
2024-10-03 07:27:19 +0000

    update django urls

--- my_tennis_club/members/url.py
+++ my_tennis_club/members/url.py
@@ -1,3 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('members/', views.members, name='members'),
+]
+
 from django.contrib import admin
 from django.urls import include, path
 
